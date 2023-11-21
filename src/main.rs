b8227f73e8b068cd15791f800f9f90408e0dcaa7
flexyuan <pyrrhosc@protonmail.com>
2023-11-21 20:06:47 +0530

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,10 @@
 use std::env;
 use std::io;
 use std::process;
-
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
-    }
-}
+use std::process::ExitCode;
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
+fn main() -> ExitCode {
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
@@ -25,10 +15,28 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    let grep = Grep {
+        pattern,
+        input: input_line,
+    };
+    if grep.is_match() {
+        ExitCode::from(0)
+    } else {
+        ExitCode::from(1)
+    }
+}
+
+struct Grep {
+    pattern: String,
+    input: String,
+}
+
+impl Grep {
+    fn is_match(&self) -> bool {
+        if self.pattern.chars().count() == 1 {
+            return self.input.contains(&self.pattern);
+        } else {
+            panic!("Unhandled pattern: {}", self.pattern)
+        }
+    }
 }
